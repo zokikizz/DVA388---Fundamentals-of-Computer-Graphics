4d64cc882faaf55bf5f95dd316f91194218494d9
kizz <zp.zoki95@gmail.com>
2018-05-23 19:07:24 +0200

    reflection, shadows and lighting

--- DVA338_lab3/Main.cpp
+++ DVA338_lab3/Main.cpp
@@ -21,6 +21,7 @@ using namespace std;
 
 Camera cam;
 Light light;
+int ray_count = 0;
 
 
 class Scene {
@@ -67,7 +68,6 @@ private:
 		return Vec3f(left + x * dx, bottom + y * dy, z).normalize();
 	}
 
-
 public:
 	SimpleRayTracer(Scene * scene, Image * image) {
 		this->scene = scene;
@@ -75,39 +75,144 @@ public:
 	}
 
 	void searchClosestHit(const Ray & ray, HitRec & hitRec) {
+		float min = ray.tMax;
+		HitRec temp;
 		for (int i = 0; i < scene->spheres.size(); i++) {
-			scene->spheres[i].hit(ray, hitRec);			
+			temp.anyHit = false;
+			scene->spheres[i].hit(ray, temp);
+
+			if (temp.anyHit && temp.tHit < min)
+			{
+				min = temp.tHit;
+				hitRec = temp;
+				hitRec.primIndex = i;
+			}
 		}
 	}
 
-	void fireRays(void) { 
+	Vec3f reflection(const Ray & ray, HitRec & hr, int count) {
+		
+		ray_count++;
+
+		if(count <= 3)
+		{
+			// hit
+			Vec3f l = (light.position - hr.p).normalize();
+			Vec3f r = (hr.n * (l.dot(hr.n)) * 2.0f - l).normalize();
+			Ray rr;
+			HitRec hrr;
+			rr.o = hr.p;
+			rr.epsMoveStartAlongSurfaceNormal(hr.n);
+			rr.d = r;
+			hrr.anyHit = false;
+			searchClosestHit(rr,hrr);
+
+			if(hrr.anyHit)
+			{
+				Sphere s = scene->spheres[hrr.primIndex];
+				Vec3f color = Vec3f(0.0f,0.0f,0.0f);
+				Ray rrr;
+				HitRec hrrr;
+				rrr.o = hrr.p;
+				rrr.epsMoveStartAlongSurfaceNormal(hrr.n);
+				rrr.d = (light.position - rrr.o).normalize();
+				hrrr.anyHit = false;
+				searchClosestHit(rrr,hrrr);
+
+				if(!hrrr.anyHit)
+					color += Phong(rr,hrr);
+
+				return color + reflection(rr, hrr, count + 1) * pow(max(ray.d.dot(r), 0.0f), s.shininess * 128.0f);
+			}
+			else
+			{
+				return Vec3f(0.0f, 0.0f, 0.0f);
+			}
+
+		}
+		else // lighting(ray, hr);
+		{
+			Vec3f color = light.Ia.multCoordwise(scene->spheres[hr.primIndex].Ka);
+			Ray rr;
+			HitRec hrr;
+			rr.o = hr.p;
+			rr.epsMoveStartAlongSurfaceNormal(hr.n);
+			rr.d = (light.position - rr.o).normalize();
+			hrr.anyHit = false;
+			searchClosestHit(rr, hrr);
+
+			if(hrr.anyHit)
+				color += Phong(ray,hr);
+
+			return color;
+		}
+	}
+
+
+	Vec3f Phong(const Ray & ray, HitRec & hr)
+	{
+		Sphere s = scene->spheres[hr.primIndex];
+		Vec3f l = (light.position - hr.p).normalize();
+		Vec3f r = ((hr.n * (l.dot(hr.n) * 2.0f)) - l).normalize();
+		Vec3f v = (cam.position - hr.p).normalize();
+		
+		Vec3f diffuse = (light.Id.multCoordwise(s.Kd * max(hr.n.dot(l), 0.0f)));
+		Vec3f specular = (light.Is.multCoordwise(s.Ks * pow(max(r.dot(v), 0.0f), s.shininess)));
+
+		return diffuse + specular;
+	}
+
+	Vec3f lighting(const Ray & ray, HitRec hr)
+	{
+		Ray shadowr;
+		HitRec shadow;
+		shadowr.o = hr.p;
+		shadowr.epsMoveStartAlongSurfaceNormal(hr.n);
+		shadowr.d = (light.position - shadowr.o).normalize();
+		shadow.anyHit = false;
+		searchClosestHit(shadowr, shadow);
+
+		Vec3f color = light.Ia.multCoordwise(scene->spheres[hr.primIndex].Ka);
+		ray_count++;
+	
+		if (!shadow.anyHit)
+			color += Phong(ray, hr);
+		
+		color += reflection(ray,hr, 1);
+		return color;
+	}
+
+	void fireRays(void) {
 		Ray ray;
 		HitRec hitRec;
 		//bool hit = false;
-		//ray.o = Vec3f(0.0f, 0.0f, 0.0f); //Set the start position of the eye rays to the origin
-		ray.o = cam.position; 
+		ray.o = cam.position; //Set the start position of the eye rays to the origin
 
-		for (int y = 0; y < image->getHeight(); y++) {
-			for (int x = 0; x < image->getWidth(); x++) {
+		for (int y = 0; y < image->getHeight(); y++)
+		{
+			for (int x = 0; x < image->getWidth(); x++)
+			{
 				ray.d = getEyeRayDirection(x, y);
 				hitRec.anyHit = false;
 				searchClosestHit(ray, hitRec);
-				if (hitRec.anyHit) {
+				Vec3f color = Vec3f(0.0f, 0.0f, 0.0f);
+				ray_count++;
 
-					Vec3f color = scene->spheres[hitRec.primIndex].color(hitRec, light, cam);
-				 
+				if (hitRec.anyHit) {
+					color += lighting(ray, hitRec);
 					image->setPixel(x, y, color);
 					glSetPixel(x, y, color);
-				} else {
-					// background black
-					image->setPixel(x, y, Vec3f(0.0f, 0.0f, 0.0f));
-					glSetPixel(x, y, Vec3f(0.0f, 0.0f, 0.0f));
+				}
+				else
+				{
+					image->setPixel(x, y, Vec3f(0,0,0));
+					glSetPixel(x, y, Vec3f(0,0,0));
+			
 				}
 			}
 		}
 	}
 };
-
 
 SimpleRayTracer * rayTracer;
 
@@ -118,6 +223,7 @@ void display(void) {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
+	ray_count = 0;
 	rayTracer->fireRays();
 	
 	max(0.0f, 1.0f);
@@ -154,6 +260,30 @@ void keypress(unsigned char key, int x, int y)
 		case 'Z':
 			cam.position.z -= 1.0f;
 			break;
+
+		case 'a':
+			light.position.x += 1.0f;
+			break;
+		case 'd':
+			light.position.x -= 1.0f;
+			break;
+		
+		case 'w':
+			light.position.z += 1.0f;
+			break;
+		case 's':
+			light.position.z -= 1.0f;
+			break;
+	
+		case 'r':
+			light.position.z += 1.0f;
+			break;
+		case 'e':
+			light.position.z -= 1.0f;
+			break;
+
+
+
 		case 'Q':
 		case 'q':
 			glutLeaveMainLoop();
@@ -182,6 +312,7 @@ void init(void)
 
 	Scene * scene = new Scene;
 	scene->add(Sphere(Vec3f(0.0f, 0.0f, -10.0f), 2.0f, 32.f, Vec3f(1.0f, 0.5f, 0.31f), Vec3f(1.0f, 0.5f, 0.31f), Vec3f(0.5f, 0.5f, 0.5f)));
+	scene->add(Sphere(Vec3f(-1.0f, -1.0f, -8.0f), 1.0f, 32.f, Vec3f(1.0f, 0.5f, 0.31f), Vec3f(1.0f, 0.5f, 0.31f), Vec3f(0.5f, 0.5f, 0.5f)));
 
 	light.position = cam.position;
 	light.Ia = Vec3f(0.2f, 0.2f, 0.2f);
